a2cc24d2a22a8ed0b54ff9368066b1254f0188c0
Denus01289@gmail.com <Denus01289@gmail.com>
2020-06-01 15:38:11 +0300

    lab2
    2

---
+++ Lab2.1/Lab2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SeeMark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double W, mStart, mFinish, mPas, vStart, vFinish, vPas = 1;
+            Console.Write("Введите начальную массу - ");
+            mStart = double.Parse(Console.ReadLine());
+            Console.Write("Введите конечную массу - ");
+            mFinish = double.Parse(Console.ReadLine());
+            Console.Write("Введите шаг изменения массы - ");
+            mPas = double.Parse(Console.ReadLine());
+            Console.Write("Введите начальную скорость - ");
+            vStart = double.Parse(Console.ReadLine());
+            Console.Write("Введите конечную скорость - ");
+            vFinish = double.Parse(Console.ReadLine());
+            Console.Write("Введите шаг изменения скорости - ");
+            vPas = double.Parse(Console.ReadLine());
+
+            while (mStart < mFinish)
+            {
+                for (double i = vStart; i < vFinish; i += vPas)
+                {
+                    W = mStart * (Math.Pow(i, 2) / 2);
+                    Console.WriteLine("W = {0} при m = {1} и v={2}", W, mStart, i);
+                }
+                mStart += mPas;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
