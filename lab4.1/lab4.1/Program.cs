22d1b82aa37c87712ed7db5be42d0dcda4585780
Denus01289@gmail.com <Denus01289@gmail.com>
2020-06-01 18:20:04 +0300

    лаб4.0

---
+++ lab4.1/lab4.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ComsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0; 
+            string line;                
+            char[] separator = { ' ', '.', '*', '+', ';', ',', '?', '!', '‐', '/' }; 
+            StringBuilder otvet = new StringBuilder();  
+            using (StreamReader MyFile = new StreamReader("text.txt"))
+            {
+                
+                while ((line = MyFile.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);  
+                                             
+                    string[] words = line.Split(separator);
+                    foreach (string slovo in words) 
+                    {
+                        if ((slovo.Length >= 2) && (slovo[1] == 'd'))
+                        {
+                            counter++;
+                            otvet.Append(slovo);
+                            otvet.Append("  ");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("There were {0} sequences.", counter);
+            Console.WriteLine(otvet.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+
+
